8540ee438a6e13b163349bf6b5838f159b7ad2ad
MilanK0 <36692511+MilanK0@users.noreply.github.com>
2019-02-27 21:01:07 +0100

    Update gauss2D.m

--- 2/Part-1_ Image Enhancement/gauss2D.m
+++ 2/Part-1_ Image Enhancement/gauss2D.m
@@ -8,4 +8,19 @@ function G = gauss2D( sigma , kernel_size )
             G(i,j) = x(i) * y(j);
         end
     end
+    
+%Used this Gauss2D for question 7.4
+%function [result] = gauss2D( sigma , kernel_size ) 
+%    %Q7.4
+%    % Generate horizontal and vertical coordinates, where the origin is in the middle
+%    ind = -floor(kernel_size/2) : floor(kernel_size/2);
+%    [X Y] = meshgrid(ind, ind);
+    
+    % Create Gaussian Mask
+%    h = exp(-(X.^2 + Y.^2) / (2*sigma*sigma));
+    
+    % Normalize so that total area (sum of all weights) is 1
+%    h = h / sum(h(:));
+%    result = h;
+%end
 end
