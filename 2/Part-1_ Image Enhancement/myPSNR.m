63b080ef337cd9b61759721eaaf9de65aef0f577
MilanK0 <36692511+MilanK0@users.noreply.github.com>
2019-02-27 20:58:36 +0100

    Update myPSNR.m

--- 2/Part-1_ Image Enhancement/myPSNR.m
+++ 2/Part-1_ Image Enhancement/myPSNR.m
@@ -1,5 +1,13 @@
 function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
+%This code calculates and prints the PSNR of two images (an original image and the
+%approximation image. 
 
+%%%% Comment the following line from approx_image (as shown in this example), when using denoise.m , because the images are
+%%%% already read. 
+orig_image = imread(orig_image);% approx_image = imread(approx_image);
+orig_image = double(orig_image);
+approx_image = double(approx_image);
+ImaxRoot = max(max(orig_image)).^2;
+d = sum((orig_image(:)-approx_image(:)).^2) / prod(size(orig_image));
+PSNR = 10*log10(ImaxRoot/d)
 end
-
